8a2de394793e7ad7ca57a5e40862d81d557305c4
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2024-03-26 09:13:16 +0100

    Create fake input on the fly

--- tests/integration/S3_integration_test.py
+++ tests/integration/S3_integration_test.py
@@ -4,11 +4,13 @@ from main_data_processor import run
 from dane.config import cfg
 import pytest
 import os
+import tarfile
 from io_util import validate_s3_uri, obtain_input_file, generate_output_dirs
 
 
 source_id = "resource__carrier"
-example_key = f"{cfg.INPUT.S3_FOLDER_IN_BUCKET}/prep__{source_id}.tar.gz"
+fn_tar = f"prep__{source_id}.tar.gz"
+example_key = f"{cfg.INPUT.S3_FOLDER_IN_BUCKET}/{fn_tar}"
 
 @pytest.fixture(scope="function")
 def aws_credentials():
@@ -21,19 +23,31 @@ def aws_credentials():
     os.environ["MOTO_S3_CUSTOM_ENDPOINTS"] = cfg.INPUT.S3_ENDPOINT_URL
 
 
-
 @pytest.fixture(scope="function")
 def aws(aws_credentials):
     with mock_aws():
         yield boto3.client("s3")
 
+
+@pytest.fixture(scope="function")
+def create_sample_input():
+    fn = '{source_id}.input'
+    with open(fn, 'w') as f:
+        f.write('This is just a file with some random input')
+    with tarfile.open(fn_tar, 'w:gz') as tar: 
+        tar.add(fn)
+    yield
+    #  after test: cleanup
+    os.remove(fn)
+    os.remove(fn_tar)
+
 @pytest.fixture
-def create_and_fill_buckets(aws):
+def create_and_fill_buckets(aws, create_sample_input):
     client = boto3.client("s3")
     for bucket in [cfg.INPUT.S3_BUCKET, cfg.OUTPUT.S3_BUCKET, cfg.INPUT.S3_BUCKET_MODEL]:
         client.create_bucket(Bucket=bucket)
-    client.put_object(Body="tests/integration/resource__carrier.input",
-                      Bucket=cfg.INPUT.S3_BUCKET, 
+    client.put_object(Body=fn_tar,  # "tests/integration/resource__carrier.input",
+                      Bucket=cfg.INPUT.S3_BUCKET,
                       Key=f"{cfg.INPUT.S3_FOLDER_IN_BUCKET}/prep__{source_id}.tar.gz")
 
 
@@ -41,7 +55,7 @@ def create_and_fill_buckets(aws):
 def setup_fs():
     generate_output_dirs(source_id)
 
-
+"""
 def test_io_util(aws, aws_credentials, create_and_fill_buckets, setup_fs):
     s3_uri = f"s3://{cfg.INPUT.S3_BUCKET}/{cfg.INPUT.S3_FOLDER_IN_BUCKET}/prep__{source_id}.tar.gz"
     assert validate_s3_uri(s3_uri=s3_uri)
@@ -53,12 +67,15 @@ def test_io_util(aws, aws_credentials, create_and_fill_buckets, setup_fs):
 
     model_input = obtain_input_file(s3_uri=s3_uri)
     assert model_input.state == 200
+"""
 
 
 def test_main_data_processor(aws, aws_credentials, create_and_fill_buckets):
     assert True
     if cfg.OUTPUT.TRANSFER_ON_COMPLETION:
+        # run the main data processor
         run(input_file_path=f"s3://{cfg.INPUT.S3_BUCKET}/{cfg.INPUT.S3_FOLDER_IN_BUCKET}/prep__{source_id}.tar.gz")
+        # Check if the output is present in S3
         client = boto3.client("s3")
         found = False
         for item in client.list_objects(Bucket=cfg.OUTPUT.S3_BUCKET)['Contents']:
