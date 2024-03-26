d6d84503577108a96571de2912e30c17cd59586c
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2024-03-26 08:45:42 +0100

    Mostly readability fixes

--- tests/integration/S3_integration_test.py
+++ tests/integration/S3_integration_test.py
@@ -17,7 +17,7 @@ def aws_credentials():
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
-    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"  # Other regions makes stuff complex
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"  # Other regions make stuff complex
     os.environ["MOTO_S3_CUSTOM_ENDPOINTS"] = cfg.INPUT.S3_ENDPOINT_URL
 
 
@@ -32,7 +32,6 @@ def create_and_fill_buckets(aws):
     client = boto3.client("s3")
     for bucket in [cfg.INPUT.S3_BUCKET, cfg.OUTPUT.S3_BUCKET, cfg.INPUT.S3_BUCKET_MODEL]:
         client.create_bucket(Bucket=bucket)
-    #assert False
     client.put_object(Body="tests/integration/resource__carrier.input",
                       Bucket=cfg.INPUT.S3_BUCKET, 
                       Key=f"{cfg.INPUT.S3_FOLDER_IN_BUCKET}/prep__{source_id}.tar.gz")
@@ -60,14 +59,14 @@ def test_main_data_processor(aws, aws_credentials, create_and_fill_buckets):
     assert True
     if cfg.OUTPUT.TRANSFER_ON_COMPLETION:
         run(input_file_path=f"s3://{cfg.INPUT.S3_BUCKET}/{cfg.INPUT.S3_FOLDER_IN_BUCKET}/prep__{source_id}.tar.gz")
-        client = boto3.client("s3")        
-
+        client = boto3.client("s3")
         found = False
         for item in client.list_objects(Bucket=cfg.OUTPUT.S3_BUCKET)['Contents']:
             found = item['Key'] == f'{cfg.OUTPUT.S3_FOLDER_IN_BUCKET}/resource__carrier/base_name__{source_id}.tar.gz'
             if found:
                 break
         assert found
+        # TODO: check that the output is not only present, but also matches expectations
     else:
         print("Not configured to transfer output!")
         assert False
